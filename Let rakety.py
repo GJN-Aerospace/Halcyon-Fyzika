e2f8b1d4f1fe1ad94bd0b40cd6e98e2ea15f0bbc
ondrejzikmund34 <57959907+ondrejzikmund34@users.noreply.github.com>
2021-04-17 23:52:40 +0200

    Update Let rakety.py
    Program obsahuje řešení problému metodou forward Euler, Runge-Kuttovou metodou 4. řádu a metodou Leap frog. Všechny metody konvergují k téže hodnotě maximální výšky.

--- Let rakety.py
+++ Let rakety.py
@@ -4,16 +4,73 @@ Created on Mon Dec 21 14:45:57 2020
 
 @author: Ondra Zikmund
 """
+def Runge_kutta4(y,v,a_1,a_2,a_3,dt):
+    
+    j_1 = dt*(a_1)
+    k_1 = dt*v
+    
+    j_2 = dt*(a_2)
+    
+    k_2 = dt*(v+j_1/2)
+    
+    j_3 = dt*(a_2)
+    k_3 = dt*(v+j_2/2)
+    
+    j_4 = dt*(a_3)
+    k_4 = dt*(v+j_2)
+    
+    dy = 1/6*(k_1+2*k_2+2*k_3+k_4)
+    dv = 1/6*(j_1+2*j_2+2*j_3+j_4)
+    
+    y = y+dy
+    v = v+dv
+    
+    return y,v
+
+def Forward_Euler(y,v,a_1,dt):
+    
+    dy = v*dt
+    dv = a_1*dt
+    
+    y = y+dy
+    v = v+dv
+    
+    return y,v
+
+def Leap_frog(y,v,a_1,dt):
+    
+    dv = a_1*dt
+    dy = (v+dv)*dt
+    
+    y = y+dy
+    v = v+dv
+    
+    return y,v
+    
+    
+
+def Thrust(t):   # thrust force as a funtion of time
+    if t>=0 and t<=0.7:
+        th = 12.86*t
+    if t>0.7 and t<=1:
+        th = -20*t + 23
+    if t>1 and t<=5.5:
+        th = 3
+    if t>5.5 and t<=6.25:
+        th = -4*t+25
+    if t>6.25:
+        th = 0
+    
+    return th
 
 import matplotlib.pyplot as plt
 
-#-------------------------------------------------#
-# ROCKET SIMULATION
-#-------------------------------------------------#
 
+# =============================================================================
+# Rocket parameters
+# =============================================================================
 # All numbers are in S.I. units
 
-# Motor parameters
 
 m_m = 0.0167
 I = 20  # total impuls
@@ -22,139 +79,114 @@ v_e = I/m_m   # velocity of products
 
 
 r = 0.0625  # radius of the rocket
-mass_r = 0.239 # inital mass of the rocket with the motors
+m = 0.239 # inital mass of the rocket with the motors
 th = 0   # initial force of thrust
 t_th = 9
-a_th  = th/mass_r  # acceleration due to thrust
-g = -9.8  # gravitational acceleration, positive direction is oriented upward
+a_th  = th/m  # acceleration due to thrust
+g = 9.8  # gravitational acceleration, positive direction is oriented upward
 dens = 1.225  # densitiy of air
-c_d = 0.35  # drag coefficient of the rocket
-s = r*r*3.14  # cross-sectional area of the rocket
+c_d = 0.65 # drag coefficient of the rocket  0,35
+S = r*r*3.14  # cross-sectional area of the rocket
+
+
+#-------------------------------------------------#
+# ROCKET SIMULATION
+#-------------------------------------------------#
+
 
 y = 0  # initial hight is 0
 v = 0 # initial velociy is 0
+t = 0
+  
 position = []  # vector of y values
 velocity = []  # vector of v  values
+
+
+dt = 0.01  # elementary time interval
+n = round(t_th/dt)# number of intervals
 
 #--------------------------------------------#
 # Accelerating phase  (thrust accelerates the rocket)
 #--------------------------------------------#
 
-dt = 0.0001  # elementary time interval
-n = t_th/dt  # number of intervals
+TH = [0]
+T = [0]
+T1 = [0]
+T2 = [0]
+V = [0]
+Y = [0]
+M = [0.239]
 
 
-n = round(n)
-
-t = 0
-y= 0
-v = 0
-
-TH = []
-T = []
-V = []
-Y = []
-
-
-for interval in range(n+1):
-       
-    if t>=0 and t<=0.7:
-        th = 12.86*t
-
-    if t>0.7 and t<1:
-        th = -20*t + 23
-    if t>1 and t<5.5:
-        th = 3
-    if t>5.5 and t<6.25:
-        th = -4*t+25
-    if t>6.25:
-        th = 0
+for interval in range(n):
+        
+    th = Thrust(t)
+    TH.append(th)
+    m -= dt*th/v_e  # as the rocket is burning fuel, it's mass decrease
+    M.append(m)
     
-    mass_r -= dt*th/v_e
-    
-    # Acceleration
-
-    a_th = th/mass_r  # acceleration due to thrust
-    
-    a_d = (- 0.5*s*c_d*dens*v*v)/mass_r  # acceleration due to drag
-    
-    a = a_th+g+a_d  # overall acceleration
-    
+    if V[-1]>0:
+        k = +1/2*dens*c_d*S
+    else:
+        k = -1/2*dens*c_d*S
+   
+    a_th1 = Thrust(t)/m  # acceleration due to thrust
+    a_th2 = Thrust(t+dt)/m
+    a_th3 = Thrust(t+2*dt)/m
     
 
-    if a<0 and y==0:
+    a_1 = a_th1-g-k*v**2 # entire acceleration
+    a_2 = a_th2-g-k*v**2 # entire acceleration
+    a_3 = a_th3-g-k*v**2 # entire acceleration
+
+
+    if a_1<=0 and y==0:
+        
         y = 0
         v = 0
         Y.append(y)
         V.append(v)
 
     else:
-        v += a*dt
-        V.append(v)
-        y += v*dt
+     #   y = Runge_kutta4(y,v,a_1,a_2,a_3,dt)[0]
+        #y = Forward_Euler(y,v,a_1,dt)[0]
+        y = Leap_frog(y,v,a_1,dt)[0]
         Y.append(y)
         
-    T.append(t)
-    TH.append(th)
-  
-    t += dt
-plt.plot(T,TH, label = 'Thrust')
-#--------------------------------------------#
-# Decelerating phase (rocket is moving up by it's inertia)
-# -------------------------------------------#
-
-t_dp = -v/g  # duration of the decelerating phase before the rocket stops in air  !!!
-
-n_2 = t_dp/dt
-n_2 = round(n_2)
-
-
-v_2 = v
-y_2 = y
-
-t +=-dt
-print(n_2)
-
-for interval_2 in range(100):
-    
-
-    f_d = -abs(v_2)/v_2*0.5*s*c_d*dens*v_2*v_2
-    v_2 += dt*(g+f_d/mass_r)
-    y_2 += dt*v_2
-    position.append(y_2)
-    velocity.append(v_2)
+       # v = Runge_kutta4(y,v,a_1,a_2,a_3,dt)[1]
+        #v = Forward_Euler(y,v,a_1,dt)[1]
+        v = Leap_frog(y,v,a_1,dt)[1]
+        V.append(v)
     t +=dt
+    T1.append(2*dt*interval)   # time for Leap frog scenario
+    T2.append(2*dt*interval+dt) # time for Leap frog scenario
     T.append(t)
-    Y.append(y_2)
-    V.append(v_2)
-
-
-times = []  # vector of time values
-for time in range(n+n_2):
-    times.append(time*dt)
-    
-a = [n for n in range(80)]
-b= [6]*80
-
 
 
 # =============================================================================
-# Analysis
+# Visualisation
 # =============================================================================
 
-print("The maximal hight was:")
+plt.close()
+print("The maximal height was:")
 print(max(Y))
 print("The maximal velocity was")
 print(max(V))
 
-plt.plot(T,V, label = 'velocity')
-plt.plot(T,Y, label = 'hight')
+#plt.plot(T,V, label = 'velocity')  # Runge-Kutta and forward Euler
+#plt.plot(T,Y, label = 'height')    # Runge-Kutta and forward Euler
+plt.plot(T,TH, label = 'thrust')
 
-plt.title("Rocket flight")
+plt.plot(T1,V, label = 'velocity')  # Leap frog
+plt.plot(T2,Y, label = 'height')    # Leap frog
+
+plt.title("Rocket flight", size=25)
 plt.xlabel("time(s)")
 plt.ylabel("Characteristics (SI)")
 
 plt.legend(loc="lower center", bbox_to_anchor=(1, 1))
+
+
 
 
     
